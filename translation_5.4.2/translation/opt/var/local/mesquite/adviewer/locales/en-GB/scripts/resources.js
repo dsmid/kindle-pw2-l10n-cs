7688c8875525d71892904a28265505a2d87d2350
lednak <dobry.rocnick@seznam.cz>
2014-02-05 16:16:59 +0100

    Update resources.js

--- translation_5.4.2/translation/opt/var/local/mesquite/adviewer/locales/en-GB/scripts/resources.js
+++ translation_5.4.2/translation/opt/var/local/mesquite/adviewer/locales/en-GB/scripts/resources.js
@@ -6,23 +6,23 @@ Device Not Registered
 Error	
 Offer Expired	Platnost nabídky vypršela
 Order Cancelled	Objednávka zrušena
-Parental Controls are enabled on your Kindle. Purchases are not allowed.	
+Parental Controls are enabled on your Kindle. Purchases are not allowed.	Na tvém Kindle je rodičovský zámek. Nákupy nejsou povoleny.
 Please connect wirelessly to download the latest Special Offers. New offers will display automatically when available.	
 Please register your Kindle to receive the latest Special Offers. New offers will display automatically when available.	
 Purchased by Accident? Cancel this Order	Zakoupeno omylem? Zrušit tuto objednávku
 Purchase Limit Reached	Bylo dosaženo nákupního limitu
-Purchasing Locked	
-Special Offer	
-Thank you for your order	
+Purchasing Locked	Nakupování uzamčeno
+Special Offer	Speciální nabídka
+Thank you for your order	Děkujemeza vaši objednávku
 To complete your AmazonLocal order, connect to a wireless network within the next {purchaseDaysRemaining} days. We will send a voucher to the Home screen and a notification to your e-mail account once your order has processed. The voucher will contain instructions on how to redeem this offer.	
 View All Special Offers	Zobrazit všechny speciální nabídky
 View Special Offers	Zobrazit speciální nabídky
 We are unable to process your request. Please try again later.	 Nejsme schopni zpracovat váš požadavek. Zkuste to později, prosím.
-You already have {count} order(s) pending for this special offer.	
+You already have {count} order(s) pending for this special offer.	Máte již {count} objednávek z této speciální nabídky.
 You have other orders pending. Click here to view	Máte další nedokončené objednávky. Klikněte sem pro zobrazení
 You have successfully cancelled your order for {dealTitle} from {merchantName}. Your credit card was not charged.	
-You must register your Kindle to make a purchase.	
+You must register your Kindle to make a purchase.	K nákupupům musíte svůj Kindle zaregistrovat.
 Your order will be placed when you connect to wireless.	Vaše objednávka bude odeslána, až se připojíte k Wi-Fi.
-Your purchase could not be completed. This offer has expired.	
+Your purchase could not be completed. This offer has expired.	Váš nákup nemohl být dokončen. Tato nabídka vypršela.
 Your purchase could not be completed. We are experiencing technical difficulties. Please try again later.	Váš nákup nemohl být dokončen kvůli technickým potížím. Zkuste to později, prosím.
 Your purchase could not be completed. You have reached the maximum purchase quantity.	Váš nákup nebylo možné dokončit. Dosáhli jste limitu pro maximální počet nakoupených položek.
