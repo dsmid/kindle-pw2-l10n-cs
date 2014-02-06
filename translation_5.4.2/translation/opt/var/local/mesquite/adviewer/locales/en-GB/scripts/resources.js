e5eae3272452d4556244106b83da13e1b1dd14c9
lednak <dobry.rocnick@seznam.cz>
2014-02-06 17:31:49 +0100

    Update resources.js

--- translation_5.4.2/translation/opt/var/local/mesquite/adviewer/locales/en-GB/scripts/resources.js
+++ translation_5.4.2/translation/opt/var/local/mesquite/adviewer/locales/en-GB/scripts/resources.js
@@ -3,25 +3,25 @@ Close	Zavřít
 Details for this advert are no longer available.	
 Details for this advert are no longer available. Turn on wireless to receive the latest content.	
 Device Not Registered	
-Error	
+Error	Chyba
 Offer Expired	Platnost nabídky vypršela
 Order Cancelled	Objednávka zrušena
-Parental Controls are enabled on your Kindle. Purchases are not allowed.	Na tvém Kindle je rodičovský zámek. Nákupy nejsou povoleny.
-Please connect wirelessly to download the latest Special Offers. New offers will display automatically when available.	
-Please register your Kindle to receive the latest Special Offers. New offers will display automatically when available.	
+Parental Controls are enabled on your Kindle. Purchases are not allowed.	Na vašem Kindle je rodičovský zámek. Nákupy nejsou povoleny.
+Please connect wirelessly to download the latest Special Offers. New offers will display automatically when available.	Připojte se prosím prostřednictvím Wi-Fi a budou staženy nejnovější speciální nabídky. Nové nabídky se ukáží automaticky, jakmile budou k dipozici.
+Please register your Kindle to receive the latest Special Offers. New offers will display automatically when available.	Zaregistrujte prosím váš Kindle, abyste obdrželi nejnovější speciální nabídky. Nové nabídky se ukáží automaticky, jakmile budou k dipozici.
 Purchased by Accident? Cancel this Order	Zakoupeno omylem? Zrušit tuto objednávku
 Purchase Limit Reached	Bylo dosaženo nákupního limitu
 Purchasing Locked	Nakupování uzamčeno
 Special Offer	Speciální nabídka
 Thank you for your order	Děkujemeza vaši objednávku
-To complete your AmazonLocal order, connect to a wireless network within the next {purchaseDaysRemaining} days. We will send a voucher to the Home screen and a notification to your e-mail account once your order has processed. The voucher will contain instructions on how to redeem this offer.	
+To complete your AmazonLocal order, connect to a wireless network within the next {purchaseDaysRemaining} days. We will send a voucher to the Home screen and a notification to your e-mail account once your order has processed. The voucher will contain instructions on how to redeem this offer.	K dokončení vaší objednávky u AmazonLocal se prosím během následujících {purchaseDaysRemaining} dní připojte k Wi-Fi. Jakmile se objednávka začne zpracovávat, pošleme vám nákupní poukaz na vaši Domovskou stránku a také upozornění e-mailem. Nákupní poukázka bude obsahovat informace, jak ji uplatnit.
 View All Special Offers	Zobrazit všechny speciální nabídky
 View Special Offers	Zobrazit speciální nabídky
 We are unable to process your request. Please try again later.	 Nejsme schopni zpracovat váš požadavek. Zkuste to později, prosím.
 You already have {count} order(s) pending for this special offer.	Máte již {count} objednávek z této speciální nabídky.
 You have other orders pending. Click here to view	Máte další nedokončené objednávky. Klikněte sem pro zobrazení
-You have successfully cancelled your order for {dealTitle} from {merchantName}. Your credit card was not charged.	
-You must register your Kindle to make a purchase.	K nákupupům musíte svůj Kindle zaregistrovat.
+You have successfully cancelled your order for {dealTitle} from {merchantName}. Your credit card was not charged.	Vaše objednávka {dealTitle} u {merchantName} byla řádně zrušena. Vaše kreditní karta nebyla použita.
+You must register your Kindle to make a purchase.	K nákupům musíte svůj Kindle zaregistrovat.
 Your order will be placed when you connect to wireless.	Vaše objednávka bude odeslána, až se připojíte k Wi-Fi.
 Your purchase could not be completed. This offer has expired.	Váš nákup nemohl být dokončen. Tato nabídka vypršela.
 Your purchase could not be completed. We are experiencing technical difficulties. Please try again later.	Váš nákup nemohl být dokončen kvůli technickým potížím. Zkuste to později, prosím.
