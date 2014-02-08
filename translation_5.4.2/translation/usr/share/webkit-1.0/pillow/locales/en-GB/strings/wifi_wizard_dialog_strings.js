ecf8f6f1d2a9e71ffc5b6078adf824a2d1b4c337
David Šmíd <david.smid@clever.cz>
2014-02-08 16:23:28 +0100

    Translated Wi-Fi setup

--- translation_5.4.2/translation/usr/share/webkit-1.0/pillow/locales/en-GB/strings/wifi_wizard_dialog_strings.js
+++ translation_5.4.2/translation/usr/share/webkit-1.0/pillow/locales/en-GB/strings/wifi_wizard_dialog_strings.js
@@ -2,21 +2,21 @@ Advanced	Pokročilé
 Advanced Options	Pokročilá nastavení
 Are you sure you want to forget the Wi-Fi network "{essid}"?	Opravdu chcete smazat profil Wi-Fi sítě "{essid}"?
 Available Networks	Dostupné sítě
-CA Certificate	
+CA Certificate	CA certifikát
 Cancel	Zrušit
 Connect	Připojit
 Connected	Připojeno
 Connecting	Připojuji
-Connection Failed	
+Connection Failed	Připojení selhalo
 Connection Type	Typ připojení
-DHCP	
+DHCP	DHCP
 Disconnect	Odpojit
-DNS	
+DNS	DNS
 Done	Hotovo
-EAP Method	
+EAP Method	EAP metoda
 Enter Again	Znovu zadejte
 Enter Other Wi-Fi Network	Zadejte další Wi-Fi síť
-Enterprise	
+Enterprise	Enterprise
 Enter Wi-Fi Network	Zadat Wi-Fi síť
 Error	Chyba
 Forget	Smazat
@@ -26,62 +26,62 @@ Incorrect Password	Nesprávné heslo
 IP Address	IP adresa
 MSCHAPv2	
 Network Name	Název sítě
-Network Name Too Long	
-None	Žádná
+Network Name Too Long	Název sítě je příliš dlouhý
+None	Žádné
 No Wi-Fi networks were found	Nebyly nalezeny žádné Wi-Fi sítě
-OK	
-Other...	
-PAP	
+OK	OK
+Other...	Jiná...
+PAP	PAP
 Password	Heslo
-Password Too Long	
-Password Too Short	
-PEAP	
-Personal	Osobní
-Phase 2 Authentication	
+Password Too Long	Heslo je příliš dlouhé
+Password Too Short	Heslo je příliš krátké
+PEAP	PEAP
+Personal	Personal
+Phase 2 Authentication	Autentizační fáze 2
 Rescan	Obnovit
-Router	
+Router	Brána
 Scan Complete	Skenování je dokončeno
 Scanning for Networks	Skenování sítí
-Security Error	
+Security Error	Chyba zabezpečení
 Security Type	Typ zabezpečení
 Set Up	Nastavit
 Static	Manuální
 Subnet Mask	Maska podsítě
-The security certificate cannot be located. Please verify that the name is correct and try again.	
+The security certificate cannot be located. Please verify that the name is correct and try again.	Nelze nalézt bezpečnostní certifikát. Prosím ujistěte se, že je jeho jméno správné a zkuste to znovu.
 Try Again	Zkusit znovu
-TTLS	
+TTLS	TTLS
 Unable to delete profile for Wi-Fi network "{essid}".	Není možné smazat profil Wi-Fi sítě "{essid}".
-Unable to Find Certificate	
+Unable to Find Certificate	Nelze nalézt certifikát
 Unable to set up the Wi-Fi network "{essid}". The network information you entered is incomplete. Please check network configuration and try setting up the network again. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	Váš Kindle není možné připojit k Wi-Fi síti "{essid}". Informace o sítí, které jste zadali jsou neúplné. Prosím, zkontrolujte síťové nastavení a zkuste nastavit síť znovu. Pokud máte problémy s připojením Kindle k Wi-Fi síti, můžete najít pomoc na www.kindle.com/support.
-Unsupported Network	
+Unsupported Network	Nepodporovaná síť
 Username	Uživatelské jméno
-Version	
-WEP	
-Wi-Fi Error	
-Wi-Fi Login	
+Version	Verze
+WEP	WEP
+Wi-Fi Error	Chyba Wi-Fi připojení
+Wi-Fi Login	Wi-Fi připojení
 Wi-Fi Networks ({numNetworks})	Wi-Fi sítě ({numNetworks})
-Wi-Fi Not Configured	
+Wi-Fi Not Configured	Wifi není nakonfigurováno
 Wi-Fi Password Required	Vyžadováno heslo k Wi-Fi
-WPA/WPA2	
-WPA2	
-WPS	
-You can set up the Wi-Fi network connection and security options for the network named below.	Zde je možno nastavit Wi-Fi připojení a zabezpečení.
-Your Kindle connected to the Wi-Fi network but could not reach the Internet. Contact your Internet Service Provider for further assistance.	
+WPA/WPA2	WPA/WPA2
+WPA2	WPA2
+WPS	WPS
+You can set up the Wi-Fi network connection and security options for the network named below.	Zde je možno nastavit Wi-Fi připojení a jeho zabezpečení.
+Your Kindle connected to the Wi-Fi network but could not reach the Internet. Contact your Internet Service Provider for further assistance.	Kindle je připojen k Wi-Fi síti, ale nemá přístup na Internet. Kontaktujte svého poskytovatele připojení pro podporu.
 Your Kindle is unable to connect to the Wi-Fi network "{essid}". Enterprise Wi-Fi networks are not supported on Kindle. Try connecting to another network. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	Váš Kindle není možné připojit k Wi-Fi síti "{essid}". Enterprise Wi-Fi síť není Kindlem podporovaná. Zkuste se připojit k jiné síti. Pokud máte problémy s připojením Kindle k Wi-Fi síti, můžete najít pomoc na www.kindle.com/support.
-Your Kindle is unable to connect to the Wi-Fi network "{essid}". --"{error}"--. Press Set Up to enter your password again or set up your network manually. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	
-Your Kindle is unable to connect to the Wi-Fi network "{essid}". Check that your security settings are correct and try again.	Váš Kindle není možné připojit k Wi-Fi síti "{essid}". Zkontrolujte, zda Vaše nastavení zabezpečení je správné a zkuste to znovu. 
+Your Kindle is unable to connect to the Wi-Fi network "{essid}". --"{error}"--. Press Set Up to enter your password again or set up your network manually. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	Váš Kindle není možné připojit k Wi-Fi síti "{essid}". --"{error}"--. Klepněte na Nastavit pro opakované zadání hesla nebo pro manuální nastavení. Pokud máte problémy s připojením Kindle k Wi-Fi síti, můžete najít pomoc na www.kindle.com/support.
+Your Kindle is unable to connect to the Wi-Fi network "{essid}". Check that your security settings are correct and try again.	Váš Kindle není možné připojit k Wi-Fi síti "{essid}". Zkontrolujte, zda vaše nastavení zabezpečení je správné a zkuste to znovu. 
 Your Kindle is unable to connect to the Wi-Fi network "{essid}". Peer-to-peer Wi-Fi networks are not supported on Kindle. Try connecting to another network. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	Váš Kindle není možné připojit k Wi-Fi síti "{essid}". Peer-to-peer síť není Kindlem podporována. Zkuste se připojit k jiné síti. Pokud máte problémy s připojením Kindle k Wi-Fi síti, můžete najít pomoc na www.kindle.com/support.
-Your Kindle is unable to connect to the Wi-Fi network "{essid}". Please check your credentials or contact your system administrator.	
-Your Kindle is unable to connect to the Wi-Fi network "{essid}". Press Set Up to enter your password again or set up your network manually. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	
-Your Kindle is unable to connect to the Wi-Fi network "{essid}". Tap the Home button, then connect to Wi-Fi again. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	Váš Kindle není možné připojit k Wi-Fi síti "{essid}".Klepněte na tlačítko Domů a potom se připojte znovu k Wi-Fi. Pokud máte problémy s připojením Kindle k Wi-Fi síti, můžete najít pomoc na www.kindle.com/support.
-Your Kindle is unable to connect to the Wi-Fi network "{essid}". The access point rejected the connection request. Check your routerʼs MAC address filtering settings and try again.	
-Your Kindle is unable to connect to the Wi-Fi network "{essid}". The network name is too long. Would you like to set up this network manually?	
-Your Kindle is unable to connect to the Wi-Fi network "{essid}". The password you entered is incorrect. Try entering your password again or setting up your network manually. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	
-Your Kindle is unable to connect to the Wi-Fi network "{essid}". The password you entered is longer than allowed by the Wi-Fi network. Try entering your password again. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	Váš Kindle není možné připojit k Wi-Fi síti "{essid}". Heslo, které jste zadali je delší než povoluje síť Wi-Fi. Zkuste Vaše heslo zadat znovu. Pokud máte problémy s připojením Kindle k Wi-Fi síti, můžete najít pomoc na www.kindle.com/support.
-Your Kindle is unable to connect to the Wi-Fi network "{essid}". The password you entered is missing one or more characters. Try entering your password again. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	Váš Kindle není možné připojit k Wi-Fi síti "{essid}". Heslu, které jste zadali chybí jeden nebo více znaků. Zkuste Vaše heslo zadat znovu. Pokud máte problémy s připojením Kindle k Wi-Fi síti, můžete najít pomoc na www.kindle.com/support.
-Your Kindle is unable to connect to the Wi-Fi network "{essid}". The security type you entered does not match the security type of the Wi-Fi network. Please set up the network manually again and select the correct security type. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	
-Your Kindle is unable to connect to the Wi-Fi network "{essid}". Try entering your username and password again. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	
-Your Kindle is unable to connect to the Wi-Fi network "{essid}" using WPS. Please check that the WPS button was tapped.	Váš Kindle není možné připojit k Wi-Fi síti "{essid}" používající WPS. Prosím zkontrolujte, zda bylo kliknuto na tlačítko WPS.
-Your Kindle is unable to connect to the Wi-Fi network "{essid}" using WPS. Would you like to set up this network manually?	
-Your Kindle is unable to connect to the Wi-Fi network. This network is not configured. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	Váš Kindle se nemůžet připojit k Wi-Fi síti. Tato síť není nakonfigurovaná. Pokud máte problémy s připojením Vašeho Kindle k Wi-Fi, můžete najít pomoc na www.kindle.com/support.
-Your Kindle is unable to connect to your local network "{essid}". You may need to check your local network. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	Váš Kindle není možné připojit k Wi-Fi síti "{essid}".Možná budete potřebovat zkontrolovat Vaší lokální síť. Pokud máte problémy s připojením Kindle k Wi-Fi síti, můžete najít pomoc na www.kindle.com/support.
+Your Kindle is unable to connect to the Wi-Fi network "{essid}". Please check your credentials or contact your system administrator.	Váš Kindle není možné připojit k Wi-Fi síti "{essid}". Prosím zkontrolujte své přihlašovací údaje nebo kontaktujte svého systémového administrátora.
+Your Kindle is unable to connect to the Wi-Fi network "{essid}". Press Set Up to enter your password again or set up your network manually. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	Váš Kindle není možné připojit k Wi-Fi síti "{essid}". Klepněte na Nastavit pro opakované zadání hesla nebo pro manuální nastavení. Pokud máte problémy s připojením Kindle k Wi-Fi síti, můžete najít pomoc na www.kindle.com/support.
+Your Kindle is unable to connect to the Wi-Fi network "{essid}". Tap the Home button, then connect to Wi-Fi again. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	Váš Kindle není možné připojit k Wi-Fi síti "{essid}". Klepněte na tlačítko Domů a potom se připojte znovu k Wi-Fi. Pokud máte problémy s připojením Kindle k Wi-Fi síti, můžete najít pomoc na www.kindle.com/support.
+Your Kindle is unable to connect to the Wi-Fi network "{essid}". The access point rejected the connection request. Check your routerʼs MAC address filtering settings and try again.	Váš Kindle není možné připojit k Wi-Fi síti "{essid}". Přístupový bod odmítl požadavek k připojení. Zkontrolujte seznam povolených MAC adres ve vašem routeru a zkuste to znovu.
+Your Kindle is unable to connect to the Wi-Fi network "{essid}". The network name is too long. Would you like to set up this network manually?	Váš Kindle není možné připojit k Wi-Fi síti "{essid}". Jméno sítě je příliš dlouhé. Přejete si nastavit toto připojení manuálně?
+Your Kindle is unable to connect to the Wi-Fi network "{essid}". The password you entered is incorrect. Try entering your password again or setting up your network manually. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	Váš Kindle není možné připojit k Wi-Fi síti "{essid}". Zadali jste nesprávné heslo. Přejete si nastavit toto připojení manuálně? Pokud máte problémy s připojením Kindle k Wi-Fi síti, můžete najít pomoc na www.kindle.com/support.
+Your Kindle is unable to connect to the Wi-Fi network "{essid}". The password you entered is longer than allowed by the Wi-Fi network. Try entering your password again. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	Váš Kindle není možné připojit k Wi-Fi síti "{essid}". Heslo, které jste zadali je delší než povoluje síť Wi-Fi. Zkuste vaše heslo zadat znovu. Pokud máte problémy s připojením Kindle k Wi-Fi síti, můžete najít pomoc na www.kindle.com/support.
+Your Kindle is unable to connect to the Wi-Fi network "{essid}". The password you entered is missing one or more characters. Try entering your password again. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	Váš Kindle není možné připojit k Wi-Fi síti "{essid}". Heslu, které jste zadali chybí jeden nebo více znaků. Zkuste vaše heslo zadat znovu. Pokud máte problémy s připojením Kindle k Wi-Fi síti, můžete najít pomoc na www.kindle.com/support.
+Your Kindle is unable to connect to the Wi-Fi network "{essid}". The security type you entered does not match the security type of the Wi-Fi network. Please set up the network manually again and select the correct security type. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	Váš Kindle není možné připojit k Wi-Fi síti "{essid}". Zadaný typ zabezpečení neodpovídá typu zabezpečení Wi-Fi sítě. Znovu prosím nastavte síť manuálně a zadejte správný typ zabezpečení. Pokud máte problémy s připojením Kindle k Wi-Fi síti, můžete najít pomoc na www.kindle.com/support.
+Your Kindle is unable to connect to the Wi-Fi network "{essid}". Try entering your username and password again. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	Váš Kindle není možné připojit k Wi-Fi síti "{essid}". Zkuste zadat vaše přihlašovací údaje znovu. Pokud máte problémy s připojením Kindle k Wi-Fi síti, můžete najít pomoc na www.kindle.com/support.
+Your Kindle is unable to connect to the Wi-Fi network "{essid}" using WPS. Please check that the WPS button was tapped.	Váš Kindle není možné připojit k Wi-Fi síti "{essid}" používající WPS. Prosím zkontrolujte, zda bylo aktivováno tlačítko WPS.
+Your Kindle is unable to connect to the Wi-Fi network "{essid}" using WPS. Would you like to set up this network manually?	Váš Kindle není možné připojit k Wi-Fi síti "{essid}" používající WPS. Chcete nastavit připojení manuálně?
+Your Kindle is unable to connect to the Wi-Fi network. This network is not configured. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	Váš Kindle se nemůžet připojit k Wi-Fi síti. Tato síť není nakonfigurovaná. Pokud máte problémy s připojením vašeho Kindle k Wi-Fi, můžete najít pomoc na www.kindle.com/support.
+Your Kindle is unable to connect to your local network "{essid}". You may need to check your local network. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	Váš Kindle není možné připojit k Wi-Fi síti "{essid}". Možná budete potřebovat zkontrolovat vaši lokální síť. Pokud máte problémy s připojením Kindle k Wi-Fi síti, můžete najít pomoc na www.kindle.com/support.
