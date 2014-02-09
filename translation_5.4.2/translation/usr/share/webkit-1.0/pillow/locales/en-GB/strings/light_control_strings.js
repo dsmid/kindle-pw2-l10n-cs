eb7346b99b4fc21e716e3443c47a7b45c78430f1
lednak <dobry.rocnick@seznam.cz>
2014-02-09 12:09:25 +0100

    Update light_control_strings.js

--- translation_5.4.2/translation/usr/share/webkit-1.0/pillow/locales/en-GB/strings/light_control_strings.js
+++ translation_5.4.2/translation/usr/share/webkit-1.0/pillow/locales/en-GB/strings/light_control_strings.js
@@ -1,13 +1,13 @@
 Advanced	Pokročilé
-Auto Brightness	
-In brightly lit rooms, use a <u>high</u> setting.	
-Max.	
+Auto Brightness	Automatický jas
+In brightly lit rooms, use a <u>high</u> setting.	V jasně osvětlených místnostech použijte nastavení <u>high</u>.
+Max.	Maximum
 Off	Vypnuto
 On	Zapnuto
-Recommended for Bright Rooms	
-Recommended for Dark Rooms	
-Recommended for Dim Rooms	
-Recommended for Overcast Conditions	
-Recommended for Sunlight	
-Use a low setting for dark rooms.	
+Recommended for Bright Rooms	Doporučeno pro světlé místnosti
+Recommended for Dark Rooms	Doporučeno pro tmavé místnosti
+Recommended for Dim Rooms	Doporušeno v šeru
+Recommended for Overcast Conditions	Doporučeno při zatažené obloze
+Recommended for Sunlight	Doporučeno na přímém slunci
+Use a low setting for dark rooms.	Použijte nízké nastavení pro tmavé místnosti.
 &#9654	
